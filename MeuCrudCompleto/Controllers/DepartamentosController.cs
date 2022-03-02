fab58a52107cc6812f40a82fcdaa04f82f750d8e
Renan Monteiro <renan.monteiro01@outlook.com>
2022-03-02 11:31:31 -0300

    Criação do projeto e da primeira View

---
+++ MeuCrudCompleto/Controllers/DepartamentosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeuCrudCompleto.Models;
+
+namespace MeuCrudCompleto.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+
+              public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { id = 1, nome = "Eletronicos" });
+            list.Add(new Departamento { id = 2, nome = "Eletro - Domesticos" });
+
+            return View(list);
+        }
+    }
+}
---
+++ MeuCrudCompleto/Models/Departamento.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MeuCrudCompleto.Models
+{
+    public class Departamento
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+    }
+}
