9534e02af7671ca826106927a35692172ac4b004
Renan Monteiro <renan.monteiro01@outlook.com>
2022-03-02 13:05:02 -0300

    Geração do Scaffoldind

--- MeuCrudCompleto/Controllers/DepartamentosController.cs
+++ MeuCrudCompleto/Controllers/DepartamentosController.cs
@@ -1,22 +1,153 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MeuCrudCompleto.Data;
 using MeuCrudCompleto.Models;
 
 namespace MeuCrudCompleto.Controllers
 {
     public class DepartamentosController : Controller
     {
+        private readonly MeuCrudCompletoContext _context;
 
-              public IActionResult Index()
+        public DepartamentosController(MeuCrudCompletoContext context)
         {
-            List<Departamento> list = new List<Departamento>();
-            list.Add(new Departamento { id = 1, nome = "Eletronicos" });
-            list.Add(new Departamento { id = 2, nome = "Eletro - Domesticos" });
+            _context = context;
+        }
 
-            return View(list);
+        // GET: Departamentos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamento.ToListAsync());
+        }
+
+        // GET: Departamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,nome")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(departamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento.FindAsync(id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,nome")] Departamento departamento)
+        {
+            if (id != departamento.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentoExists(departamento.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var departamento = await _context.Departamento.FindAsync(id);
+            _context.Departamento.Remove(departamento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartamentoExists(int id)
+        {
+            return _context.Departamento.Any(e => e.id == id);
         }
     }
 }
---
+++ MeuCrudCompleto/Data/MeuCrudCompletoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MeuCrudCompleto.Models;
+
+namespace MeuCrudCompleto.Data
+{
+    public class MeuCrudCompletoContext : DbContext
+    {
+        public MeuCrudCompletoContext (DbContextOptions<MeuCrudCompletoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MeuCrudCompleto.Models.Departamento> Departamento { get; set; }
+    }
+}
