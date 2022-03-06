9017b6261b8ee22cb76b4b50f5eaa918e8b350f0
Renan Monteiro <renan.monteiro01@outlook.com>
2022-03-06 20:09:41 -0300

    Criação das telas de pesquisa de Vendas

---
+++ MeuCrudCompleto/Controllers/RegistroDeVendasController.cs
@@ -0,0 +1,30 @@
+﻿using MeuCrudCompleto.Servicos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuCrudCompleto.Controllers
+{
+    public class RegistroDeVendasController : Controller
+    {
+               
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        {
+         
+            return View();
+        }
+
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ MeuCrudCompleto/Servicos/RegistroDeVendasServico.cs
@@ -0,0 +1,57 @@
+﻿using MeuCrudCompleto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MeuCrudCompleto.Data;
+
+namespace MeuCrudCompleto.Servicos
+{
+    public class RegistroDeVendasServico
+    {
+        private readonly MeuCrudCompletoContext _context;
+
+        public RegistroDeVendasServico(MeuCrudCompletoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<RegistroDeVendas>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.RegistroDeVendas select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Vendedor)
+                .Include(x => x.Vendedor.Departamento)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Departamento, RegistroDeVendas>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.RegistroDeVendas select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Vendedor)
+                .Include(x => x.Vendedor.Departamento)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Vendedor.Departamento)
+                .ToListAsync();
+        }
+    }
+}
